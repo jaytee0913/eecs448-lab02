84b8bf0ad58b52d7f68d6371ee04e6cf4f3ae705
Jeromy Tsai <jeromy.tsai@gmail.com>
2016-02-05 15:57:06 -0600

    Fix size() method

--- linkedList/LinkedList.hpp
+++ linkedList/LinkedList.hpp
@@ -23,16 +23,17 @@ LinkedList<T>::~LinkedList()
 template <typename T>
 bool LinkedList<T>::isEmpty() const
 {
+	//return correct size
+	/** TODO 
+		Fix this method
+	*/
 	return(m_size == 0);
 }
 
 template <typename T>
 int LinkedList<T>::size() const
 {
-	/** TODO 
-		Fix this method
-	*/
-	return(0);
+	return m_size;
 }
 
 template <typename T>
